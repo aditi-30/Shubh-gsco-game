007ad74a708bcac5a9cfda92cc7424164a00d144
Aditi Raut <69860437+aditi-30@users.noreply.github.com>
2020-11-11 20:47:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function draw() {
   background(0);  
   drawSprites();
 
- if (wall.x - car.x < car.width/+ wall.width/2){
+ if (wall.x - car.x < (car.width+ wall.width)/2){
    car.velocityX = 0
    var deformation = 0.5*weight*speed*speed/22500
    if(deformation < 100){
